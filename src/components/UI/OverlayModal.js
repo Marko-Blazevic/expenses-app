418f66c29c9c99da580bfd5924800ef720a39d05
Marko Blazevic <www.markoblazevic@gmail.com>
2021-07-11 13:14:00 -0300

    Corrected spelling

--- src/components/UI/OverlayModal.js
+++ src/components/UI/OverlayModal.js
@@ -10,7 +10,7 @@ const OverlayModal = (props) => {
           <h2>There was a mistake</h2>
         </header>
         <div className={classes.message}>
-          <p>Please fill in all the fields</p>
+          <p>Please fill in all fields</p>
         </div>
         <footer className={classes.footer}>
           <Button onClick={props.onClick}>Ok</Button>
